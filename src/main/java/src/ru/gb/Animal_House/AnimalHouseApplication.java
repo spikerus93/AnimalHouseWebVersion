4b5ca64d3f3e53e19ab74b5b23fa5d29717fdb66
master <spikerus93@rambler.ru>
2024-10-19 16:57:38 +0300

    создание проекта на Maven

---
+++ src/main/java/src/ru/gb/Animal_House/AnimalHouseApplication.java
@@ -0,0 +1,13 @@
+package src.ru.gb.Animal_House;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/src/ru/gb/Animal_House/AnimalHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package src.ru.gb.Animal_House;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
