250e7e8a5cd608b55ea9c2875c11edb59b171fd1
master <spikerus93@rambler.ru>
2024-10-20 18:52:53 +0300

    пересоздание класса для разделения животных - HomeAnimal

--- src/main/java/src/ru/gb/Animal_House/model/animalclass/homeAnimal/HomeAnimal.java
+++ src/main/java/src/ru/gb/Animal_House/model/animalclass/homeAnimal/HomeAnimal.java
@@ -1,12 +1,18 @@
 package src.ru.gb.Animal_House.model.animalclass.homeAnimal;
 
-import src.ru.gb.Animal_House.model.Animal;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
-public abstract class HomeAnimal extends Animal {
-    public HomeAnimal(String name, LocalDate birthDate) {
-        super(name, birthDate);
-        this.animalClass = ("Домашнее животное, Семейство: ");
+@Getter
+public abstract class HomeAnimal{
+    protected String name;
+    protected LocalDate birthDate;
+
+    public HomeAnimal(String name, LocalDate birthDate){
+        this.name = name;
+        this.birthDate = birthDate;
     }
+
+    public abstract String getAnimalClass();
 }
